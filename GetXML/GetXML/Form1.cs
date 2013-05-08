6ab0ad851d2ecb6648e5936639e83cd5b6116b1d
Luis Ribeiro <ribeiro.luis@hotmail.com>
2013-05-08 14:02:59 -0300

    Exemplo para baixar o xml em windows forms

---
+++ GetXML/GetXML/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml.Linq;
+using System.Threading;
+using System.IO;
+
+namespace GetXML
+{
+    public partial class Form1 : Form
+    {
+        WebClient wc = new WebClient();        
+        ManualResetEvent handle = new ManualResetEvent(true);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"Lista.xml"))
+            {
+                File.Delete(@"Lista.xml");
+            }
+
+            //var proxy = Proxy();
+            //wc.Proxy = proxy;
+            
+            wc.DownloadProgressChanged += WcOnDownloadProgressChanged;
+            wc.DownloadFileCompleted += WcOnDownloadFileCompleted;
+            wc.DownloadFileAsync(new Uri(@"http://alcsistemas.heliohost.org/Arquivos/Lista.xml"), @"Lista.xml");
+            handle.WaitOne(); // wait for the async event to complete
+        }
+        
+        void WcOnDownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (!e.Cancelled && e.Error == null)
+            {
+                //async download completed successfully
+            }
+            else
+                MessageBox.Show(e.Error.ToString());
+            handle.Set(); // in both the case let the void main() know that async event had finished so that i can quit
+        }
+
+        void WcOnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // handle the progres in case of async
+            //e.ProgressPercentage
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private WebProxy Proxy()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.pucminas.br");
+
+            IWebProxy proxy = request.Proxy;
+            if (proxy != null)
+            {
+                MessageBox.Show("Proxy: " +  proxy.GetProxy(request.RequestUri).ToString());
+            }
+            else
+            {
+                Console.WriteLine("Proxy is null; no proxy will be used");
+            }
+
+            WebProxy myProxy = new WebProxy();
+            Uri newUri = new Uri(proxy.GetProxy(request.RequestUri).ToString());
+            // Associate the newUri object to 'myProxy' object so that new myProxy settings can be set.
+            myProxy.Address = newUri;
+            // Create a NetworkCredential object and associate it with the 
+            // Proxy property of request object.
+
+            login log = new login();
+            log.ShowDialog();
+            
+
+            myProxy.Credentials = new NetworkCredential(log.txUsuario.Text, log.txSenha.Text);
+            request.Proxy = myProxy;
+            if (myProxy.Credentials == null)
+                return null;
+            else
+                return myProxy;
+        }
+    }
+}
+            
---
+++ GetXML/GetXML/login.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace GetXML
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txSenha = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txUsuario
+            // 
+            this.txUsuario.Location = new System.Drawing.Point(114, 38);
+            this.txUsuario.Name = "txUsuario";
+            this.txUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txUsuario.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txSenha
+            // 
+            this.txSenha.Location = new System.Drawing.Point(114, 81);
+            this.txSenha.Name = "txSenha";
+            this.txSenha.PasswordChar = '*';
+            this.txSenha.Size = new System.Drawing.Size(100, 20);
+            this.txSenha.TabIndex = 2;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 206);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txSenha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txUsuario);
+            this.Name = "login";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox txUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox txSenha;
+    }
+}
