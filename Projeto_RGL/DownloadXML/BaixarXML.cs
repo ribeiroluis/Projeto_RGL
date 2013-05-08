878f1441f0be9e7d3c7b2c41ee6376b4d440bd83
Luis Ribeiro <ribeiro.luis@hotmail.com>
2013-05-08 15:46:02 -0300

    baixando e lendo xml

---
+++ Projeto_RGL/DownloadXML/BaixarXML.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.IO.IsolatedStorage;
+using System.IO;
+
+namespace Projeto_RGL.DownloadXML
+{
+    public class BaixarXML
+    {
+        
+        
+        public BaixarXML()
+        {
+            var file = IsolatedStorageFile.GetUserStoreForApplication();
+            WebClient client = new WebClient();
+            file.CreateDirectory("Listas");
+            client.OpenReadCompleted += new OpenReadCompletedEventHandler(client_OpenReadCompleted);
+            client.OpenReadAsync(new Uri("http://alcsistemas.heliohost.org/Arquivos/Lista.xml"));
+        }
+        void client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            var file = IsolatedStorageFile.GetUserStoreForApplication();
+
+            using (IsolatedStorageFileStream stream = new IsolatedStorageFileStream("Lista.xml", FileMode.Create, file))
+            {
+                byte[] buffer = new byte[1024];
+                while (e.Result.Read(buffer, 0, buffer.Length) > 0)
+                {
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
--- Projeto_RGL/MainPage.xaml.cs
+++ Projeto_RGL/MainPage.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Projeto_RGL.DownloadXML;
+using Projeto_RGL.LerXML;
+
 
 namespace Projeto_RGL
 {
@@ -20,5 +23,35 @@ namespace Projeto_RGL
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BaixarXML baixa = new BaixarXML();
+                MessageBox.Show("Baixado com sucesso");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);                
+            }
+            
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ReadXML ler = new ReadXML();
+                scrollViewer1.
+                txBoxXML.Text = ler.LerXML();
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show(err.Message);
+            }
+        }
+        
     }
 }
